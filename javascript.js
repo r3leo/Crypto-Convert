03668de1743d9f1af4877d9faedc2312014a3e54
joelvj <joelcristobal97@gmail.com>
2021-11-20 02:53:20 -0500

    added some js -juul

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 let input;
+const crypto = document.querySelector("#crypto");
 
 function getValue() {
   input = document.querySelector(".input").value;
   console.log(input);
+}
+
+function changeToBtc() {
+  crypto.innerText = "BTC";
+}
+function changeToEth() {
+  crypto.innerText = "ETH";
+}
+function changeToSol() {
+  crypto.innerText = "SOL";
 }
